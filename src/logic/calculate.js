77ea92364e5574abd48b3130e24c8b5fca85f8b7
nati2323 <nati2.man.3@gmail.com>
2022-10-29 14:44:32 +0300

    Update calculate and operate js file

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -28,13 +28,13 @@ export default function calculate(obj, buttonName) {
     }
     // If there is an operation, update next
     if (obj.operation) {
-      if (obj.next) {
+      if (obj.next && obj.next !== '0') {
         return { ...obj, next: obj.next + buttonName };
       }
       return { ...obj, next: buttonName };
     }
     // If there is no operation, update next and clear the value
-    if (obj.next) {
+    if (obj.next && obj.next !== '0') {
       return {
         next: obj.next + buttonName,
         total: null,
@@ -54,32 +54,24 @@ export default function calculate(obj, buttonName) {
       return { ...obj, next: `${obj.next}.` };
     }
     if (obj.operation) {
-      return { next: '0.' };
+      return { ...obj, next: '0.' };
     }
     if (obj.total) {
       if (obj.total.includes('.')) {
         return {};
       }
-      return { total: `${obj.total}.` };
+      return { ...obj, next: `${obj.total}.` };
     }
-    return { total: '0.' };
+    return { ...obj, next: '0.' };
   }
 
   if (buttonName === '=') {
     if (obj.next && obj.operation) {
-      try{
-        return {
-          total: operate(obj.total, obj.next, obj.operation),
-          next: null,
-          operation: null,
-        };
-      }catch(err){
-        return {
-          total: "Can't make modulo of 0!",
-          next: null,
-          operation: null,
-        };
-      }
+      return {
+        total: operate(obj.total, obj.next, obj.operation),
+        next: null,
+        operation: null,
+      };
     }
     // '=' with no operation, nothing to do
     return {};
@@ -113,20 +105,16 @@ export default function calculate(obj, buttonName) {
     if (obj.total && !obj.next) {
       return { ...obj, operation: buttonName };
     }
-    try {
-      return {
-        total: operate(obj.total, obj.next, obj.operation),
-        next: null,
-        operation: buttonName,
-      };
-    }catch(err){
-      return {
-        total: "Can't make modulo of 0!",
-        next: null,
-        operation: buttonName,
-      };
-    }
     
+    if (!obj.total) {
+      return { total: 0, operation: buttonName };
+    }
+
+    return {
+      total: operate(obj.total, obj.next, obj.operation),
+      next: null,
+      operation: buttonName,
+    };
   }
 
   // no operation yet, but the user typed one
