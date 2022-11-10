e294baa4e1e817b62e8e77ea4179b4455cbda4dc
nati2323 <nati2.man.3@gmail.com>
2022-11-10 16:26:37 +0300

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './pages/App.css';
-import Calculator from './components/calculator';
+import Calculator from './components/Calculator';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
